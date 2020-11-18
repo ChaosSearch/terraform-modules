139607bfd716704a0ccc3f61a8b141a16f88d7f0
John Diggins <john@chaossumo.io>
2020-11-18 18:55:04 -0500

    Updating to Terraform v0.13

--- encrypted-s3-bucket-live-indexing/variables.tf
+++ encrypted-s3-bucket-live-indexing/variables.tf
@@ -1,16 +1,21 @@
 variable "region" {
   description = "Default region"
+  type        = string
   default     = "us-east-1"
 }
 
 variable "cs_external_id" {
   description = "chaossearch.io - String: External id (customer id) for bucket creations and assume role"
+  type        = string
 }
 
 variable "cs_data_bucket" {
   description = "chaossearch.io - String: Bucket name to be created to index data out of"
+  type        = string
 }
 
 variable "sqs_queue" {
   description = "chaossearch.io - Boolean: Should there be an sqs queue configured for live update"
+  type        = bool
+  default     = true
 }
